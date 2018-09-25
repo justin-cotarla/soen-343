4cdf82ca61f8fdd399724addcf0dfbc49b67f2fd
zacharybys <zachary.bys@gmail.com>
2018-09-25 17:05:55 -0400

    Added basic empty viewusers component

---
+++ frontend/src/components/ViewUsers.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import UserList from './UserList';
+import '../styles/ViewUsers.css';
+
+class ViewUsers extends Component {
+    render() {
+        const userList = [{'lastName':'Bys', 'firstName':'Zach', 'address':'4 Coronation', 'email':'zbys@gmail.com'},
+        {'lastName':'Bys', 'firstName':'Zach', 'address':'4 Coronation', 'email':'zbys@gmail.com'},
+        {'lastName':'Bys', 'firstName':'Zach', 'address':'4 Coronation', 'email':'zbys@gmail.com'},
+        {'lastName':'Bys', 'firstName':'Zach', 'address':'4 Coronation', 'email':'zbys@gmail.com'}]
+        
+        return (
+            <div className="view-users-container">
+                <h1 className="view-users-title">Logged in Users</h1>
+                <UserList users={userList}/>
+            </div>
+        );
+    }
+}
+
+export default ViewUsers;
