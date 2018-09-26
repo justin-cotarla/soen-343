839630d88b2144ab8fff9d52f1299afd459e7f6d
laurawheatley <laura.eliza.wheatley@gmail.com>
2018-09-26 17:50:25 -0400

    [#9] Fixing base 64 decoding and adding 400 response status messages

--- backend/app/accounts/AccountService.ts
+++ backend/app/accounts/AccountService.ts
@@ -6,23 +6,23 @@ class AccountService {
     async login(request: Request, response: Response) {
         
         if (!request.get('Authorization')) {
-            response.status(400);
+            response.status(400).send('Missing Authorization');
             return;
         }
 
-        const credentials = atob(request.get('Authorization')).split(':');
+        const credentials = (Buffer.from(request.get('Authorization').split(' ')[1], 'base64').toString('utf8')).split(':');
         const email = credentials[0];
         const password = credentials[1];
 
         if (!email) {
-            response.status(400);
+            response.status(400).send('Missing Email');
             return;
         }
         if (!password) {
-            response.status(400);
+            response.status(400).send('Missing Password');
             return;
         }
-        
+
         try {
             const authUser = await authenticate(email, password);
             const token = await generateToken(authUser);
