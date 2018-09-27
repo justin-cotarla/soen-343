e64f048ca77dc27f482e83968296807dc5b5686b
Derek Yu <derekyu624@hotmail.com>
2018-09-26 23:27:28 -0400

    [#9] Add Client and Administrator classes to AuthUtil

--- backend/app/models/Client.ts
+++ backend/app/models/Client.ts
@@ -1,17 +1,21 @@
 class Client {
-	public firstName: string;
-	public lastName: string;
-	public phone: int;
-	public email: string;
-	public address: string;
-	
-	constructor (firstName: string, lastName: string, phone: int, email: string, address:string){
-		this.firstName = firstName
-		this.lastName = lastName;
-		this.phone=phone;
-		this.email = email;
-		this.address = address;
+    public firstName: string;
+    public lastName: string;
+    public phone: number;
+    public email: string;
+    public address: string;
+
+    constructor (
+        firstName: string,
+        lastName: string,
+        phone: number,
+        email: string,
+        address: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phone = phone;
+        this.email = email;
+    	this.address = address;
     }
 }
-export {Client};
-
+export { Client };
