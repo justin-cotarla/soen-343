d775ada7be320684026a8dfac92322daf066a897
laurawheatley <laura.eliza.wheatley@gmail.com>
2018-09-27 12:49:41 -0400

    [#9] Fixed responses, changed request unpacking on AccountService.ts

--- backend/app/controllers/AccountService.ts
+++ backend/app/controllers/AccountService.ts
@@ -7,33 +7,20 @@ class AccountService {
 
     async login(request: Request, response: Response) {
 
-        if (!request.get('Authorization')) {
-            response.status(400).send('Missing Authorization');
-            return;
-        }
-
-        const credentials = (Buffer.from(request.get('Authorization').split(' ')[1], 'base64')
-            .toString('utf8'))
-            .split(':');
-        const email = credentials[0];
-        const password = credentials[1];
+        const { email, password } = request.body;
 
         if (!email || !password) {
-            response.status(400).send('Incomplete form');
-            return;
+            return response.status(401).end();
         }
 
         try {
             const authUser = await authenticate(email, password);
-            let isAdmin = false;
-            if (authUser instanceof Administrator) {
-                isAdmin = true;
-            }
+            const isAdmin = (authUser instanceof Administrator);
             const token = await generateToken({ authUser, isAdmin });
             return response.status(200).json({ token });
         } catch (err) {
             console.log(`error: ${err}`);
-            response.status(400).send('Unknown Error');
+            return response.status(400).end();
         }
     }
 
@@ -41,8 +28,7 @@ class AccountService {
         const { firstName, lastName, address, phone, email, password, isAdmin } = request.body;
 
         if (!email || !password || !firstName || !lastName || !address || !phone || !isAdmin) {
-            response.status(400).send('Missing information');
-            return;
+            return response.status(400).end();
         }
 
         let client : Client;
@@ -54,10 +40,10 @@ class AccountService {
         try {
             const registerUser = await register(client, password);
             const token = await generateToken({ registerUser });
-            response.status(200).json({ token });
+            return response.status(200).json({ token });
         } catch (err) {
             console.log(`error: ${err}`);
-            response.status(400).send('Unknown Error');
+            return response.status(400).end();
         }
     }
 
