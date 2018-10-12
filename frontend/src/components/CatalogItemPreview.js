722994c9ed333f6981d4a2e31dbc81c858f5a9e9
georgikbarsemian <georgik.barsemian@mail.concordia.ca>
2018-10-12 15:38:12 -0400

    Creating functional Catalog component class and CatalogIremPreview component class

--- frontend/src/components/CatalogItemPreview.js
+++ frontend/src/components/CatalogItemPreview.js
@@ -1,21 +1,19 @@
 import React from 'react'
-import { Image, List } from 'semantic-ui-react'
+import { Icon, List } from 'semantic-ui-react'
 
 class CatalogItemPreview extends React.Component {
     
     render() {
         return (
-            <List celled>
                 <List.Item>
-                <Image file alternate outline/>
+                <Icon name="book"/>
                 <List.Content>
                     <List.Header>Snickerdoodle</List.Header>
                     An excellent companion
                 </List.Content>
                 </List.Item>
-            </List>
         );
     }
 }
 
-export default CataCatalogItemPreviewlog;
+export default CatalogItemPreview;
