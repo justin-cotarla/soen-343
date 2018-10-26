c0f121a8995d5c8e4b9ac3f28f393ce925d28370
georgikbarsemian <georgik.barsemian@mail.concordia.ca>
2018-10-25 20:43:40 -0400

    implementation of the insert and find method for the MusicTDG class

--- backend/app/persistence/MusicTDG.ts
+++ backend/app/persistence/MusicTDG.ts
@@ -6,10 +6,68 @@ import DatabaseUtil from '../utility/DatabaseUtil';
 class MusicTDG implements TableDataGateway {
 
     find = async(id: string): Promise<Music> => {
-            return null;      
+
+        try {
+            const query = `
+                SELECT
+                ID, TITLE, DATE, TYPE, ARTIST, LABEL, ASIN
+                FROM CATALOG_ITEM
+                INNER JOIN MOVIE ON CATALOG_ITEM.ID = MUSIC.CATALOG_ITEM_ID
+                WHERE ID = ?;
+            `;
+
+             const data = await DatabaseUtil.sendQuery(query, [id]);
+            if (!data.rows.length) {
+                return null;
+            }
+
+             const music = data.rows.map(music =>
+                new Music(
+                    music.ID,
+                    music.TITLE,
+                    music.DATE,
+                    music.TYPE,
+                    music.ARTIST,
+                    music.LABEL,
+                    music.ASIN,
+                ));
+             return music[0];
+        } catch (err) {
+            console.log(`error: ${err}`);
+            return null;
+        }    
     }
     insert = async(item: Music): Promise<boolean> => {
-            return false;   
+
+        try {
+            const queryCatalogItem = `
+                INSERT INTO CATALOG_ITEM
+                (TITLE, DATE)
+                VALUES
+                (?, ?);
+            `;
+            const queryMusic = `
+                INSERT INTO MUSIC
+                (TYPE, ARTIST, LABEL, ASIN)
+                VALUES
+                (?, ?, ?, ?);
+            `;
+
+             await DatabaseUtil.sendQuery(queryCatalogItem, [
+                item.title,
+                item.date]);
+
+             await DatabaseUtil.sendQuery(queryMusic, [
+                item.type,
+                item.artist,
+                item.label,
+                item.asin]);
+            
+            return true;
+        } catch (err) {
+            console.log(`error: ${err}`);
+            return null;
+        }
     }
     update = async (item: Music): Promise<boolean> => {
             return false;       
