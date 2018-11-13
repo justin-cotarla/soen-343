4677562b3f864a00f76396a6cb46f02a884444fe
Derek Yu <derekyu624@hotmail.com>
2018-11-12 22:52:44 -0500

    Write complete test coverage for catalog controller

--- backend/app/services/Catalog.ts
+++ backend/app/services/Catalog.ts
@@ -18,29 +18,29 @@ export enum CatalogItemType {
 class Catalog {
     viewItems = async (queryParam: string, order: string, direction: string, type?: CatalogItemType)
     : Promise<CatalogItem[]> => {
-        
+
         let items: CatalogItem[];
-       
-        switch(type) {
-            case CatalogItemType.BOOK:
-                items = await BookTDG.findAll(queryParam);
-                break;
-            case CatalogItemType.MUSIC:
-                items = await MusicTDG.findAll(queryParam);
-                break;
-            case CatalogItemType.MAGAZINE:
-                items = await MagazineTDG.findAll(queryParam);
-                break;
-            case CatalogItemType.MOVIE:
-                items = await MovieTDG.findAll(queryParam);  
-                break;
-            default:
-                items = [
-                    ...(await BookTDG.findAll(queryParam)),
-                    ...(await MagazineTDG.findAll(queryParam)),
-                    ...(await MovieTDG.findAll(queryParam)),
-                    ...(await MusicTDG.findAll(queryParam)),
-                ];
+
+        switch (type) {
+        case CatalogItemType.BOOK:
+            items = await BookTDG.findAll(queryParam);
+            break;
+        case CatalogItemType.MUSIC:
+            items = await MusicTDG.findAll(queryParam);
+            break;
+        case CatalogItemType.MAGAZINE:
+            items = await MagazineTDG.findAll(queryParam);
+            break;
+        case CatalogItemType.MOVIE:
+            items = await MovieTDG.findAll(queryParam);
+            break;
+        default:
+            items = [
+                ...(await BookTDG.findAll(queryParam)),
+                ...(await MagazineTDG.findAll(queryParam)),
+                ...(await MovieTDG.findAll(queryParam)),
+                ...(await MusicTDG.findAll(queryParam)),
+            ];
         }
 
         if (order === 'title' && direction === 'asc') {
--- backend/app/tests/CatalogController.test.ts
+++ backend/app/tests/CatalogController.test.ts
@@ -3,7 +3,7 @@ import supertest from 'supertest';
 
 import server from '../server';
 import { BookFormat, Book } from '../models/Book';
-import { Administrator, Magazine, InventoryItem } from '../models';
+import { Administrator, Magazine, InventoryItem, CatalogItem } from '../models';
 import { generateToken } from '../utility/AuthUtil';
 
 import Catalog from '../services/Catalog';
@@ -20,7 +20,7 @@ beforeEach(async () => {
         'Test',
         'Test',
         '12345',
-        );
+    );
     token = await generateToken({ user, isAdmin: true });
     jest.clearAllMocks();
 });
@@ -28,9 +28,36 @@ beforeEach(async () => {
 describe('CatalogRouter', () => {
     describe('PUT /catalog', () => {
         it('requires admin rights', async () => {
-            supertest(server)
-                .put('/catalog')
+            await supertest(server)
+                .put('/catalog/book')
                 .expect(403);
+        });
+
+        it('handles invalid type', async (done) => {
+            const bookRequest = {
+                catalogItem: {
+                    title: 'TestBook',
+                    date: 'TestDate',
+                    isbn10: 1234567890,
+                    isbn13: 123456789012,
+                    author: 'TestAuthor',
+                    publisher: 'TestPublisher',
+                    format: BookFormat.HardCover,
+                    pages: 69,
+                },
+                quantity: 1,
+            };
+
+            supertest(server)
+                .put('/catalog/invalid')
+                .set('Authorization', `Bearer ${token}`)
+                .set('Accept', 'application/json')
+                .send(bookRequest)
+                .expect(400)
+                .end((err: any, res: any) => {
+                    if (err) return done(err);
+                    done();
+                });
         });
 
         it('does not accept empty requests', async (done) => {
@@ -63,6 +90,36 @@ describe('CatalogRouter', () => {
                 .set('Authorization', `Bearer ${token}`)
                 .send(invalidRequest)
                 .expect(400)
+                .end((err: any, res: any) => {
+                    if (err) return done(err);
+                    done();
+                });
+        });
+
+        it('handles bad request', async (done) => {
+            Catalog.addItem = jest.fn(() => {
+                throw new Error();
+            });
+            const bookRequest = {
+                catalogItem: {
+                    title: 'TestBook',
+                    date: 'TestDate',
+                    isbn10: 1234567890,
+                    isbn13: 123456789012,
+                    author: 'TestAuthor',
+                    publisher: 'TestPublisher',
+                    format: BookFormat.HardCover,
+                    pages: 69,
+                },
+                quantity: 1,
+            };
+
+            supertest(server)
+                .put('/catalog/book')
+                .set('Authorization', `Bearer ${token}`)
+                .set('Accept', 'application/json')
+                .send(bookRequest)
+                .expect(500)
                 .end((err: any, res: any) => {
                     if (err) return done(err);
                     done();
@@ -165,6 +222,11 @@ describe('CatalogRouter', () => {
             'English',
         );
 
+        it('requires admin rights', async () => {
+            await supertest(server)
+                .get('/catalog')
+                .expect(403);
+        });
         it('Returns a list of all catalog items', async () => {
             Catalog.viewItems = jest.fn().mockReturnValueOnce([magazine]);
             const response = await supertest(server)
@@ -173,6 +235,202 @@ describe('CatalogRouter', () => {
                 .expect(200);
 
             expect(response.body.length).toEqual(1);
+        });
+        it('handles bad request error', async () => {
+            Catalog.viewItems = jest.fn(() => {
+                throw new Error();
+            });
+            await supertest(server)
+                .get('/catalog')
+                .set('Authorization', `Bearer ${token}`)
+                .expect(400);
+        });
+    });
+
+    describe('GET /catalog/:type', () => {
+        const magazine: Magazine = new Magazine(
+            'test',
+            'Popular Mechanics',
+            'Test Date',
+            123,
+            123,
+            'Test Pub',
+            'English',
+        );
+
+        it('requires admin rights', async () => {
+            await supertest(server)
+                .get('/catalog/magazine')
+                .expect(403);
+        });
+        it('successfully gets all magazine types', async () => {
+            Catalog.viewItems = jest.fn().mockReturnValueOnce([magazine]);
+            const response = await supertest(server)
+                .get('/catalog/magazine')
+                .set('Authorization', `Bearer ${token}`)
+                .expect(200);
+
+            expect(response.body.length).toEqual(1);
+            expect(response.body[0].catalogItemType).toEqual('magazine');
+        });
+        it('handles invalid type', async () => {
+            await supertest(server)
+                .get('/catalog/invalid')
+                .set('Authorization', `Bearer ${token}`)
+                .expect(400);
+        });
+        it('handles bad request error', async () => {
+            Catalog.viewItems = jest.fn(() => {
+                throw new Error();
+            });
+            await supertest(server)
+                .get('/catalog/magazine')
+                .set('Authorization', `Bearer ${token}`)
+                .expect(500);
+        });
+    });
+
+    describe('GET /catalog/:type/:id', () => {
+        const magazine: Magazine = new Magazine(
+            'test',
+            'Popular Mechanics',
+            'Test Date',
+            123,
+            123,
+            'Test Pub',
+            'English',
+        );
+
+        it('requires admin rights', async () => {
+            await supertest(server)
+                .get('/catalog/magazine/test')
+                .expect(403);
+        });
+        it('successfully gets the magazine with specified id', async () => {
+            Catalog.viewItem = jest.fn().mockReturnValueOnce(magazine);
+            const response = await supertest(server)
+                .get('/catalog/magazine/test')
+                .set('Authorization', `Bearer ${token}`)
+                .expect(200);
+
+            expect(response.body.catalogItem).toEqual(magazine);
+        });
+        it('handles invalid type', async () => {
+            await supertest(server)
+                .get('/catalog/invalid/test')
+                .set('Authorization', `Bearer ${token}`)
+                .expect(400);
+        });
+        it('handles non-existent catalog item with specified id', async () => {
+            Catalog.viewItem = jest.fn().mockReturnValueOnce(null);
+            await supertest(server)
+                .get('/catalog/magazine/test')
+                .set('Authorization', `Bearer ${token}`)
+                .expect(404);
+        });
+        it('handles bad request error', async () => {
+            Catalog.viewItem = jest.fn(() => {
+                throw new Error();
+            });
+            await supertest(server)
+                .get('/catalog/magazine/test')
+                .set('Authorization', `Bearer ${token}`)
+                .expect(500);
+        });
+    });
+
+    describe('POST /catalog/:type/:id', () => {
+        const bookRequest = {
+            catalogItem: {
+                title: 'TestBook',
+                date: 'TestDate',
+                isbn10: 1234567890,
+                isbn13: 123456789012,
+                author: 'TestAuthor',
+                publisher: 'TestPublisher',
+                format: BookFormat.HardCover,
+                pages: 69,
+            },
+        };
+
+        it('requires admin rights', async () => {
+            await supertest(server)
+                .post('/catalog/magazine/test')
+                .expect(403);
+        });
+        it('Creates a catalog item of the specified type', async () => {
+            const updatedItem: Book = {
+                id: '1',
+                title: 'TestBook',
+                date: 'TestDate',
+                isbn10: 1234567890,
+                isbn13: 123456789012,
+                author: 'TestAuthor',
+                publisher: 'TestPublisher',
+                format: BookFormat.HardCover,
+                pages: 69,
+            };
+
+            Catalog.updateItem = jest.fn().mockReturnValueOnce(updatedItem);
+
+            const response = await supertest(server)
+                .post('/catalog/book/1')
+                .set('Authorization', `Bearer ${token}`)
+                .set('Accept', 'application/json')
+                .send(bookRequest)
+                .expect(200);
+
+            expect(response.body).toEqual(updatedItem);
+        });
+        it('handles invalid type', async (done) => {
+            supertest(server)
+                .post('/catalog/invalid/test')
+                .set('Authorization', `Bearer ${token}`)
+                .set('Accept', 'application/json')
+                .send(bookRequest)
+                .expect(400)
+                .end((err: any, res: any) => {
+                    if (err) return done(err);
+                    done();
+                });
+        });
+        it('throws an error if a single attribute is missing', async (done) => {
+            const invalidRequest = {
+                catalogItem: {
+                    title: 'TestBook',
+                    date: 'TestDate',
+                    isbn10: 1234567890,
+                    isbn13: 123456789012,
+                    author: 'TestAuthor',
+                    publisher: 'TestPublisher',
+                    format: BookFormat.HardCover,
+                },
+            };
+            supertest(server)
+                .post('/catalog/book/test')
+                .set('Authorization', `Bearer ${token}`)
+                .send(invalidRequest)
+                .expect(400)
+                .end((err: any, res: any) => {
+                    if (err) return done(err);
+                    done();
+                });
+        });
+        it('handles bad request', async (done) => {
+            Catalog.updateItem = jest.fn(() => {
+                throw new Error();
+            });
+
+            supertest(server)
+                .post('/catalog/book/test')
+                .set('Authorization', `Bearer ${token}`)
+                .set('Accept', 'application/json')
+                .send(bookRequest)
+                .expect(500)
+                .end((err: any, res: any) => {
+                    if (err) return done(err);
+                    done();
+                });
         });
     });
 
@@ -186,11 +444,27 @@ describe('CatalogRouter', () => {
         });
 
         it('throws an error if a non-existing catalogItem is requested', async (done) => {
+            Catalog.addInventoryItem = jest.fn().mockReturnValueOnce(null);
             supertest(server)
-                .put('/catalog/bad/inventory')
+                .put('/catalog/book/bad/inventory')
                 .set('Authorization', `Bearer ${token}`)
                 .send()
                 .expect(404)
+                .end((err: any, res: any) => {
+                    if (err) return done(err);
+                    done();
+                });
+        });
+
+        it('handles bad request', async (done) => {
+            Catalog.addInventoryItem = jest.fn(() => {
+                throw new Error();
+            });
+            supertest(server)
+                .put(`/catalog/book/${catalogItemId}/inventory`)
+                .set('Authorization', `Bearer ${token}`)
+                .send()
+                .expect(500)
                 .end((err: any, res: any) => {
                     if (err) return done(err);
                     done();
@@ -214,6 +488,18 @@ describe('CatalogRouter', () => {
             await supertest(server)
                 .delete('/catalog/magazine/1')
                 .expect(403);
+        });
+
+        it('handles invalid type', async (done) => {
+            supertest(server)
+                .delete('/catalog/invalid/1')
+                .set('Authorization', `Bearer ${token}`)
+                .send()
+                .expect(400)
+                .end((err: any, res: any) => {
+                    if (err) return done(err);
+                    done();
+                });
         });
 
         it('throws an error if trying to delete a non-existing catalog item', async (done) => {
