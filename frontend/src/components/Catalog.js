292af5fa0a7038d9ada374638d7ebded963f7e80
tehblasian <jeremiahdavid.wreh@gmail.com>
2018-11-13 22:04:28 -0500

    [#115] Initial commit of type filter

--- frontend/src/components/Catalog.js
+++ frontend/src/components/Catalog.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import { Grid, List, Header, Select } from 'semantic-ui-react'
+import { Grid, List } from 'semantic-ui-react'
 import { Route } from 'react-router-dom';
 
+import CatalogTypeFilter from '../components/CatalogTypeFilter';
 import CatalogItem from '../components/CatalogItem';
 import CatalogItemPreview from "../components/CatalogItemPreview";
 
@@ -16,12 +17,11 @@ class Catalog extends React.Component {
         query: '',
         order: '',
         direction: '',
-     };
+    };
 
     componentDidMount = async () => {
         const { location: { state } } = this.props;
-
-        let query, type, order;
+        let { type, query, order, direction } = this.state;
         if (state) {
             query = state.query;
             type = state.type;
@@ -29,7 +29,7 @@ class Catalog extends React.Component {
         }
 
         try {
-            const { data } = await getCatalog(type, query, order, this.state.direction);
+            const { data } = await getCatalog(type, query, order, direction);
             this.setState({ 
                 catalog: data,
                 type,
@@ -40,6 +40,12 @@ class Catalog extends React.Component {
 
         }
     }
+
+    handleTypeFilterClick = (e, { value }) => this.setState({ type: value }, async () => {
+        const { type, query, order, direction } = this.state;
+        const { data } = await getCatalog(type, query, order, direction);
+        this.setState({ catalog: data });
+    });
 
     handleDropdownChange = async (e, { name, value }) => {
         const { type, query } = this.state;
@@ -85,34 +91,18 @@ class Catalog extends React.Component {
     }
     
     render() {
-        const options = [
-            { text: 'Oldest', value: 'Oldest' },
-            { text: 'Newest', value: 'Newest' },
-            { text: 'A-Z', value: 'A-Z' },
-            { text: 'Z-A', value: 'Z-A' },
-        ];
-
         const { match, location } = this.props;
-        const { catalog } = this.state;
+        const { catalog, type } = this.state;
         return (
             <div style={{
                 display: 'inline-block',
                 width: '100%',
                 marginTop: '4em',
-                padding: '0 1em' }}>
-                <Header 
-                    as='h1' 
-                    color='teal' 
-                    textAlign='left' 
-                    style={{ display: 'inline-block', margin: '1em 0', width: 'fit-content'}}>
-                    Catalog
-                </Header>
-                <Select  
-                    name='direction'
-                    options={options} 
-                    placeholder='Order By'
-                    onChange={this.handleDropdownChange}
-                    style={{ position: 'absolute', right: 0, margin: '2em'}}/>
+                padding: '1em 2em' }}>
+                <CatalogTypeFilter 
+                    selectedTypeFilter={type} 
+                    handleTypeFilterClick={this.handleTypeFilterClick}
+                    handleDropdownChange={this.handleDropdownChange}/>
                 <Grid textAlign='center' stackable>
                     <Grid.Column width={location.pathname.match(/^\/catalog(\/(book|magazine|movie|music))?\/?$/) ? 16 : 10} floated="left">
                         <List 
