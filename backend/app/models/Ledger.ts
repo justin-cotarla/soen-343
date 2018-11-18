b7cbc25d14012663ab71b41156b88dc6da082768
Derek Yu <derekyu624@hotmail.com>
2018-11-17 21:13:09 -0500

    [#128] Create Ledger class

---
+++ backend/app/models/Ledger.ts
@@ -0,0 +1,27 @@
+import { Transaction, OperationType } from './Transaction';
+import { TransactionTDG } from '../persistence';
+
+class Ledger {
+    addTransaction = async (operation: OperationType, userId: string, itemId: string) => {
+        const transaction = new Transaction(
+            '',
+            Date.now().toString(),
+            operation,
+            userId,
+            itemId,
+        );
+        await TransactionTDG.insert(transaction);
+    }
+
+    viewTransactions = async (
+        query: string,
+        order: string,
+        direction: string,
+        timestamp: string,
+        operation: OperationType,
+        ) => {
+
+    }
+}
+
+export default new Ledger();
--- backend/app/models/index.ts
+++ backend/app/models/index.ts
@@ -3,9 +3,11 @@ import { Book } from './Book';
 import { CatalogItem } from './CatalogItem';
 import { Client } from './Client';
 import { InventoryItem } from './InventoryItem';
+import Ledger from './Ledger';
 import { Magazine } from './Magazine';
 import { Movie } from './Movie';
 import { Music } from './Music';
+import { Transaction } from './Transaction';
 import { User } from './User';
 
 export {
@@ -14,8 +16,10 @@ export {
     CatalogItem,
     Client,
     InventoryItem,
+    Ledger,
     Magazine,
     Movie,
     Music,
+    Transaction,
     User,
 };
