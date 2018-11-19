c784693cfe365e29690b7105a205418e52b1f960
tehblasian <jeremiahdavid.wreh@gmail.com>
2018-11-19 14:00:15 -0500

    [#125] Add @ensures to TransactionService

--- backend/app/services/TransactionService.ts
+++ backend/app/services/TransactionService.ts
@@ -3,6 +3,9 @@ import { Cart, InventoryItem } from '../models';
 class TransactionService {
     carts: Map<string, Cart>;
 
+    // @ensures({
+    //    this.carts !== null,
+    // })
     constructor() {
         this.carts = new Map<string, Cart>();
     }
@@ -12,7 +15,7 @@ class TransactionService {
         //     this.carts.get(userId) !== null,
         // })
         // @ensures({
-        //     - cart.getItems() === @pre cart.getItems()
+        //     cart.getItems() === @pre cart.getItems()
         // })
         const cart = this.carts.get(userId);
         if (cart === undefined) {
