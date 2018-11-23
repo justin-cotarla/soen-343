9eaff5b6a5e5045aef6f1951edabc9e49e0b7940
Unknown <jamal.g16@hotmail.com>
2018-11-22 20:52:23 -0500

    [#146] Dummy Data functional

--- database/schema.sql
+++ database/schema.sql
@@ -72,7 +72,8 @@ ENGINE = InnoDB;
 -- Table `library_db`.`MAGAZINE`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `library_db`.`MAGAZINE` (
-  `ASIN` VARCHAR(45) NOT NULL,
+  `ISBN_10` CHAR(10) NOT NULL,
+  `ISBN_13` CHAR(13) NOT NULL,
   `PUBLISHER` VARCHAR(45) NOT NULL,
   `LANGUAGE` VARCHAR(45) NOT NULL,
   `CATALOG_ITEM_ID` INT NOT NULL,
